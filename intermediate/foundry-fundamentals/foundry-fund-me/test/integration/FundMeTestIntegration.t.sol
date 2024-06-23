8ce43bfb0bcb3c97c2e205733cc142ffd5b9ae2f
jklpdgoat <jarpadul@gmail.com>
2024-06-24 03:46:27 +0800

    Add More Tests; Fix foundry-devops module not working with forge; Did a manual submodule add

---
+++ intermediate/foundry-fundamentals/foundry-fund-me/test/integration/FundMeTestIntegration.t.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../../src/FundMe.sol";
+import {DeployFundMe} from "../../script/DeployFundMe.s.sol";
+
+contract FundMeTestIntegration is Test {
+    function setUp() external {
+        DeployFundMe deploy = new DeployFundMe();
+
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
+    }
+}
