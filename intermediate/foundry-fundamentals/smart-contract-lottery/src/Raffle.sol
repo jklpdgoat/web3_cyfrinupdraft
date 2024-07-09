1a86c61259b82330387bc861c3aad621c9bc2117
jklpdgoat <jarpadul@gmail.com>
2024-07-10 02:17:09 +0800

    Lesson 8

--- intermediate/foundry-fundamentals/smart-contract-lottery/src/Raffle.sol
+++ intermediate/foundry-fundamentals/smart-contract-lottery/src/Raffle.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+// import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFConsumerBaseV2Plus} from "lib/chainlink-brownie-contracts/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
 
 /**
  * @title A sample Raffle contract
@@ -13,8 +14,6 @@ contract Raffle is VRFConsumerBaseV2Plus {
     /* Errors */
     error Raffle__SendMoreToEnterRaffle();
 
-    /* Events */
-    event RaffleEntered(address indexed player);
 
     uint256 private immutable i_entranceFee;
     // @dev The duration of lottery in seconds
@@ -22,10 +21,14 @@ contract Raffle is VRFConsumerBaseV2Plus {
     address payable[] private s_players;
     uint256 s_lastTimeStamp;
 
-    constructor(uint256 entranceFee, uint256 interval) {
+    /* Events */
+    event RaffleEntered(address indexed player);
+
+    constructor(uint256 entranceFee, uint256 interval, address vrfCoordinator) VRFConsumerBaseV2Plus(vrfCoordinator) {
         i_entranceFee = entranceFee;
         i_interval = interval;
         s_lastTimeStamp = block.timestamp;
+        s_vrfCoordinator.requestRandomWords();
     }
 
     function enterRaffle() public payable {
@@ -55,24 +58,26 @@ contract Raffle is VRFConsumerBaseV2Plus {
         // Get our random number 2.5
         // 1. Request RNG
         // 2. Get RNG
-        requestId = s_vrfCoordinator.requestRandomWords(
-            VRFV2PlusClient.RandomWordsRequest({
-                keyHash: s_keyHash,
-                subId: s_subscriptionId,
-                requestConfirmations: requestConfirmations,
-                callbackGasLimit: callbackGasLimit,
-                numWords: numWords,
-                extraArgs: VRFV2PlusClient._argsToBytes(
-                    // Set nativepayment to true to pay for VRF requests with Sepolia ETH instead
-                    VRFV2PlusClient.ExtraAngsV1({nativepayment: false})
-                )
-            })            
-        );
+        // requestId = s_vrfCoordinator.requestRandomWords(
+        //     VRFV2PlusClient.RandomWordsRequest({
+        //         keyHash: s_keyHash,
+        //         subId: s_subscriptionId,
+        //         requestConfirmations: requestConfirmations,
+        //         callbackGasLimit: callbackGasLimit,
+        //         numWords: numWords,
+        //         extraArgs: VRFV2PlusClient._argsToBytes(
+        //             // Set nativepayment to true to pay for VRF requests with Sepolia ETH instead
+        //             VRFV2PlusClient.ExtraAngsV1({nativepayment: false})
+        //         )
+        //     })            
+        // );
 
         // s_rollers[requestId] = roller;
         // s_results[rolller] = ROLL_IN_PROGRESS;
         // emit DiceRolled(requestId, roller);
     }
+
+    function fulfillRandomWords(uint256 requestId, uint256[] calldata randomWords) internal override {}
 
     function getEntranceFee() external view returns (uint256) {
         return i_entranceFee;
